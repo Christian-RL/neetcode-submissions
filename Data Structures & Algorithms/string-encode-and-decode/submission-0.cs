97b083169ef53c2d0c7dad7115bddd2a69afbc30
Christian Ruihi-Lewis <hamaka1206@gmail.com>
2026-09-18 18:28:42 +1200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        StringBuilder bld = new StringBuilder();
+        foreach(string s in strs){
+            int length = s.Length;
+            bld.Append(length + "#" + s);
+        }
+        return bld.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> results = new();
+        List<char> chars = s.ToCharArray().ToList();
+        int i = 0;
+        while(i < chars.Count()){
+            StringBuilder bld = new StringBuilder();
+            StringBuilder length = new StringBuilder();
+            while(chars[i] != '#'){
+                length.Append(chars[i]);
+                i++;
+            }
+            string count = length.ToString();
+            int num = int.Parse(count);
+            i++;
+            for(int c = 0; c < num; c++){
+                bld.Append(chars[i]);
+                i++;
+            }
+            String res = bld.ToString();
+            results.Add(res);
+        }
+        return results;
+   }
+}
