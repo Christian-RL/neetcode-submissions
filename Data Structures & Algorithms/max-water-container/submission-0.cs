7550611185791c476d9b277cfb1ae830cda2f351
Christian Ruihi-Lewis <hamaka1206@gmail.com>
2026-09-20 23:09:03 +1200

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int pointer1 = 0;
+        int pointer2 = heights.Length - 1;
+        int max = 0;
+        int vol = 0;
+        while(pointer1 < pointer2){
+            vol = Math.Min(heights[pointer1], heights[pointer2]) * (pointer2 - pointer1);
+            if(heights[pointer1] > heights[pointer2]){
+                if(max < vol) max = vol;
+                pointer2--;
+            }else if(heights[pointer1] <= heights[pointer2]){
+                if(max < vol) max = vol;
+                pointer1++;
+            }
+        }
+        return max;
+    }
+}
